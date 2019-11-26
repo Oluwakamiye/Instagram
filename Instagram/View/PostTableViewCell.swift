28822b5e6c91c534147ff2b0ad2ef606477a3da6
Akindele Oluwakamiye <karmmie1@gmail.com>
2019-11-26 07:51:17 +0100

    Initial Commit

---
+++ Instagram/View/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Oluwakamiye Akindele on 26/11/2019.
+//  Copyright © 2019 Oluwakamiye Akindele. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Oluwakamiye Akindele on 25/11/2019.
+//  Copyright © 2019 Oluwakamiye Akindele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
